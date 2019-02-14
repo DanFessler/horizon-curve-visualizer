9b59e6f4b116eff113cfa79782a49423c14a979f
DanFessler <fezzinate@gmail.com>
2019-02-14 12:50:06 -0800

    Updates

---
+++ precache-manifest.bacd86209c53b7d97c9477e38770f43a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd2e6fec0e59d0e76bf6",
+    "url": "/horizon-curve-visualizer/static/js/runtime~main.bd2e6fec.js"
+  },
+  {
+    "revision": "270b20d8fc65e1c0b026",
+    "url": "/horizon-curve-visualizer/static/js/main.270b20d8.chunk.js"
+  },
+  {
+    "revision": "2c568cb7638f283b53bc",
+    "url": "/horizon-curve-visualizer/static/js/1.2c568cb7.chunk.js"
+  },
+  {
+    "revision": "270b20d8fc65e1c0b026",
+    "url": "/horizon-curve-visualizer/static/css/main.9c46138a.chunk.css"
+  },
+  {
+    "revision": "e5688283efcdcb29f4938d9157af217b",
+    "url": "/horizon-curve-visualizer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/horizon-curve-visualizer/precache-manifest.76e3deb0cba97ff3bdf74a5f25ab5faa.js"
+  "/horizon-curve-visualizer/precache-manifest.bacd86209c53b7d97c9477e38770f43a.js"
 );
 
 workbox.clientsClaim();
